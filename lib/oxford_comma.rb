02dd2d6b17acc5d18bbff013db086750634c1f87
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-08-13 23:39:17 +0300

    commit tree bfa3b6df995784303650366182b2ba3f941fd424

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+    if array.length == 1
+        array.pop()
+    elsif array.length == 2
+        array.join(" and ")
+    elsif array.length >= 3
+        arr = array.pop()
+        array.join(", ") + ", and " + arr
+    else
+        puts "Hey! this is not captured!"
+    end
 
 end
